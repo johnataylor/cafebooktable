3dbb40e82ee503667798b8896003355ed9f3c215
John Taylor <johtaylo@microsoft.com>
2018-05-07 16:52:03 -0700

    cafe bt book table initial commit

---
+++ CafeTableBooker/BookTableBot.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+
+namespace CafeTableBooker
+{
+    public class BookTableBot : IBot
+    {
+        private DialogSet _dialogs;
+
+        public BookTableBot()
+        {
+            _dialogs = new DialogSet();
+
+            _dialogs.Add("booking", new BookTableControl());
+            _dialogs.Add("firstRun",
+                new WaterfallStep[]
+                {
+                    async (dc, args, next) =>
+                    {
+                         await dc.Context.SendActivity("Welcome! We need to ask a few questions to get started.");
+                         await dc.Begin("booking",
+                             new Dictionary<string, object> { { "message", "hello world" } });
+                    },
+                    async (dc, args, next) =>
+                    {
+                        // Note the use of ToNaturalLanguage() which will give friendly strings like "tomorrow" "yesterday" etc.
+                        // It takes a datetime arg because the friendly strings are relative to a time - generally the current time
+
+                        var timexProperty = new TimexProperty(args["bookingDateTime"].ToString());
+                        var bookingDateTime = $"{timexProperty.ToNaturalLanguage(DateTime.Now)}";
+                        var bookingLocation = args["bookingLocation"].ToString();
+                        var bookingGuestCount = args["bookingGuestCount"].ToString();
+
+                        await dc.Context.SendActivity($"Thanks, I have {bookingGuestCount} guests booked for our {bookingLocation} location for {bookingDateTime}.");
+                        await dc.End();
+                    }
+                }
+            );
+        }
+
+        public async Task OnTurn(ITurnContext turnContext)
+        {
+            try
+            {
+                switch (turnContext.Activity.Type)
+                {
+                    case ActivityTypes.Message:
+                        var state = ConversationState<Dictionary<string, object>>.Get(turnContext);
+                        var dc = _dialogs.CreateContext(turnContext, state);
+
+                        await dc.Continue();
+
+                        if (!turnContext.Responded)
+                        {
+                            await dc.Begin("firstRun");
+                        }
+
+                        break;
+
+                    case ActivityTypes.ConversationUpdate:
+                        foreach (var newMember in turnContext.Activity.MembersAdded)
+                        {
+                            if (newMember.Id != turnContext.Activity.Recipient.Id)
+                            {
+                                await turnContext.SendActivity("Hello and welcome to the book a table bot.");
+                            }
+                        }
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                await turnContext.SendActivity($"Exception: {e.Message}");
+            }
+        }
+    }
+}
---
+++ CafeTableBooker/BookTableControl.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Prompts;
+using Microsoft.Bot.Builder.Prompts.Choices;
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+
+namespace CafeTableBooker
+{
+    public class BookTableControl : DialogContainer
+    {
+        public BookTableControl()
+            : base("order")
+        {
+            var promptOptions = new ChoicePromptOptions
+            {
+                Choices = new List<Choice>
+                {
+                    new Choice { Value = "Seattle" },
+                    new Choice { Value = "Bellevue" },
+                    new Choice { Value = "Renton" },
+                }
+            };
+
+            Dialogs.Add("order",
+                new WaterfallStep[]
+                {
+                    async (dc, args, next) =>
+                    {
+                        // The dictionary passed into Begin is available here as the args
+                        // if you want anything saved from that then add it to the State
+
+                        dc.ActiveDialog.State = new Dictionary<string, object>();
+                        await dc.Prompt("timexPrompt", "When would you like to arrive? (We open at 4PM.)",
+                            new PromptOptions { RetryPromptString = "Please pick a date in the future and a time in the evening." });
+                    },
+                    async (dc, args, next) =>
+                    {
+                        var timexResult = (TimexResult)args;
+                        dc.ActiveDialog.State["bookingDateTime"] = timexResult.Resolutions.First();
+                        await dc.Prompt("choicePrompt", "Which of our locations would you like?", promptOptions);
+                    },
+                    async (dc, args, next) =>
+                    {
+                        var choiceResult = (FoundChoice)args["Value"];
+                        dc.ActiveDialog.State["bookingLocation"] = choiceResult.Value;
+
+                        await dc.Prompt("numberPrompt", "How many in your party?");
+                    },
+                    async (dc, args, next) =>
+                    {
+                        dc.ActiveDialog.State["bookingGuestCount"] = args["Value"];
+                        await dc.End(dc.ActiveDialog.State);
+                    }
+                }
+            );
+            // TimexPrompt is a very close copy of the existing DateTimePrompt - but manages to correctly handle multiple resolutions
+            Dialogs.Add("timexPrompt", new TimexPrompt(Culture.English, TimexValidator));
+            Dialogs.Add("choicePrompt", new Microsoft.Bot.Builder.Dialogs.ChoicePrompt(Culture.English) { Style = ListStyle.Inline });
+            Dialogs.Add("numberPrompt", new Microsoft.Bot.Builder.Dialogs.NumberPrompt<int>(Culture.English));
+        }
+
+        // The notion of a Validator is a standard pattern across all the Prompts
+        private static Task TimexValidator(ITurnContext context, TimexResult value)
+        {
+            var cadidates = value.Resolutions;
+
+            var constraints = new[] { TimexCreator.ThisWeek(), TimexCreator.NextWeek(), TimexCreator.Evening };
+
+            var resolutions = TimexRangeResolver.Evaluate(cadidates, constraints);
+
+            if (resolutions.Count == 0)
+            {
+                value.Resolutions = new string[] {};
+                value.Status = PromptStatus.OutOfRange;
+            }
+            else
+            {
+                value.Resolutions = new[] { resolutions.First().TimexValue };
+                value.Status = PromptStatus.Recognized;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
